0b0f28625adc468e74cce49c8a76d24b693737bd
childlala <peterpanflyinsky@gmail.com>
2015-06-18 12:18:06 +0000

    static page

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,6 @@
 class Event < ActiveRecord::Base
     validates :name, length: { maximum: 50 },
                     presence: true
+    validates :user_id, presence: true
     belongs_to :user
 end
